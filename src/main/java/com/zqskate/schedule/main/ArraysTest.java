093c35e00873c891ac14c9b66df449ebff3a229b
soho.chan <soho.chan@cs.hkbn.com.hk>
2020-10-10 14:53:22 +0800

    update

---
+++ src/main/java/com/zqskate/schedule/main/ArraysTest.java
@@ -0,0 +1,16 @@
+package com.zqskate.schedule.main;
+
+import java.util.Arrays;
+
+/**
+ * @author soho.chan
+ * @date 27/9/2020 15:34
+ * @description TODO
+ */
+public class ArraysTest {
+    public static void main(String[] args) {
+        String[] str = {"1", "2"};
+        Object[] array = {new Object(), new Object()};
+        System.out.println(Arrays.toString(array));
+    }
+}
